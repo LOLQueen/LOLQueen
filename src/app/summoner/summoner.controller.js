b614ebe72139a8a107f39a4fff1b9c54f7282ac2
Mandish Shah <shahmandish95@gmail.com>
2015-02-19 22:41:16 -0500

    Fetching RecentMatches and Summoner Info from Riot API

--- src/app/summoner/summoner.controller.js
+++ src/app/summoner/summoner.controller.js
@@ -1,10 +1,26 @@
 'use strict';
 
 angular.module('lolqueen')
-  .controller('SummonerCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
-
-  	console.log($stateParams);
+  .controller('SummonerCtrl', [
+  	'$scope', 
+  	'$stateParams',
+  	'Summoner', 
+  	'RecentMatches', 
+  	function ($scope, $stateParams, Summoner, RecentMatches) {
 
   	$scope.summonerName = $stateParams.summonerName;
+
+  	Summoner.findOne({summonerName: $scope.summonerName}).$promise
+  		.then(function(summoner) {
+  			$scope.summoner = summoner;
+  			return RecentMatches.find({summonerId: summoner.id}).$promise;
+  		})
+  		.then(function(recentMatches){
+  			$scope.recentMatches = recentMatches;
+  			return recentMatches;
+  		})
+  		.catch(function(error){
+  			console.log(error);
+  		})
 
 }]);
