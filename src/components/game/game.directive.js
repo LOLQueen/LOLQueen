9179b194eab9a3a3999f331c772c7a672d3165ad
Mandish Shah <shahmandish95@gmail.com>
2015-04-03 13:50:20 -0400

    Modified the directive to store the data needed on the scope.

--- src/components/game/game.directive.js
+++ src/components/game/game.directive.js
@@ -10,6 +10,29 @@ function lqGame(){
     templateUrl: 'components/game/game.html',
     scope: {
       game: '='
+    },
+    link: function(scope, element, attr) {
+      var game = scope.game;
+      var player = game.participants[0];
+      scope.winner = player.stats.winner;
+      scope.gameLength = game.matchDuration;
+      scope.playedAgo = (Date.now() - game.matchCreation) / 1000;
+      scope.queueType = game.queueType;
+      scope.summonerSpells = [player.spell1Id, player.spell2Id];
+      scope.championId = player.championId;
+      scope.kda = {kills: player.stats.kills, deaths: player.stats.deaths, assists: player.stats.assists};
+      scope.champLevel = player.stats.champLevel;
+      scope.goldEarned = player.stats.goldEarned;
+      scope.items = [
+        player.stats.item0,
+        player.stats.item1,
+        player.stats.item2,
+        player.stats.item3,
+        player.stats.item4,
+        player.stats.item5
+      ];
+
+      console.log(scope);
     }
   }
 }
