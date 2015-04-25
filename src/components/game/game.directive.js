61b1a20fcd4d403737e051b9f1d69fef13b29e9a
Mandish Shah <shahmandish95@gmail.com>
2015-04-25 15:11:17 -0400

    Changed the 'MoreDetails' to load on click instead of loading it with page load

--- src/components/game/game.directive.js
+++ src/components/game/game.directive.js
@@ -17,98 +17,9 @@ function lqGame(Champion, SummonerSpell, Match, $filter){
       var summoner = scope.summoner;
       var stats = game.stats;
 
+      console.log(game);
+
       var id = game.gameId;
-
-      Match.findOne({matchId: id})
-      .$promise
-      .then(function(resource){
-
-        var data = resource;
-
-        if(id == '1794640277'){
-          console.log(resource);          
-        }
-
-        return data.participants.map(function(player){
-            var champId = player.championId;
-            var teamId = player.teamId;
-
-            if(champId == game.championId){
-              player.summonerId = summoner.id;
-              player.summonerName = summoner.name;              
-            }
-
-            game.fellowPlayers.forEach(function(summoner){
-              if(summoner.championId == champId && summoner.teamId == teamId){
-                player.summonerId = summoner.summonerId;
-              }              
-            });
-
-            return player;
-          });
-
-      })
-      .then(function(participants){
-
-        //console.log(participants);
-
-          scope.otherPlayers = participants.map(function(participant){
-            var stats = participant.stats;
-            var player = {
-              champion: {id: participant.championId},
-              summonerSpells: [{ id: participant.spell1Id } , {id : participant.spell2Id} ],
-              summonerName: participant.summonerId,
-              teamId: participant.teamId,
-              role: participant.timeline.lane,
-              kills: stats.kills,
-              deaths: stats.deaths,
-              assists: stats.assists,
-              champLevel: stats.champLevel,
-              creeps: {
-                ownJungle: stats.neutralMinionsKilledTeamJungle,
-                enemyJungle: stats.neutralMinionsKilledEnemyJungle,
-                minions: stats.minionsKilled
-              },
-              goldEarned: stats.goldEarned,
-              items: [
-                stats.item0,
-                stats.item1,
-                stats.item2,
-                stats.item3,
-                stats.item4,
-                stats.item5
-              ],
-              trinket: stats.item6,
-              damageDealt: stats.totalDamageDealtToChampions,
-              wardsPlaced: stats.wardsPlaced,
-              wardsKilled: stats.wardsKilled,
-              winner: stats.winner
-            }
-
-            // get chamption name from api
-            Champion.findOne({championId: player.champion.id})
-              .$promise
-              .then(function(resource){
-                player.champion.name = resource.name;
-                player.champion.key = resource.key;
-              });
-
-            // get summoner spells
-            player.summonerSpells.forEach(function(spell){
-              SummonerSpell.findOne({spellId: spell.id})
-                .$promise
-                .then(function(resource){
-                  spell.name = resource.name;
-                  spell.key = resource.key;
-                  spell.description = resource.description;
-                });
-            });  
-
-            //console.log(player);
-            return player;
-        });
-
-      });
 
       scope.won = stats.win;
       scope.gameLength = stats.timePlayed;
@@ -163,12 +74,21 @@ function lqGame(Champion, SummonerSpell, Match, $filter){
           });
       });      
 
+      var haveMatchInfo = false;
+
       scope.toggleDetails = function(){
         if (scope.details) {
           scope.details = false;
 
           //show info
         } else {
+          
+          if(!haveMatchInfo){
+            getMatchInfo()
+            .then(function(){
+              haveMatchInfo = true;
+            });
+          }
           scope.details = true;
         }
       }
@@ -176,6 +96,101 @@ function lqGame(Champion, SummonerSpell, Match, $filter){
       scope.details = false;
 
       //console.log(scope);
+
+      function getMatchInfo(){
+
+        return Match.findOne({matchId: id})
+        .$promise
+        .then(function(resource){
+
+          var data = resource;
+
+          if(id == '1794640277'){
+            console.log(resource);          
+          }
+
+          return data.participants.map(function(player){
+              var champId = player.championId;
+              var teamId = player.teamId;
+
+              if(champId == game.championId){
+                player.summonerId = summoner.id;
+                player.summonerName = summoner.name;              
+              }
+
+              game.fellowPlayers.forEach(function(summoner){
+                if(summoner.championId == champId && summoner.teamId == teamId){
+                  player.summonerId = summoner.summonerId;
+                }              
+              });
+
+              return player;
+            });
+
+        })
+        .then(function(participants){
+
+          //console.log(participants);
+
+            scope.otherPlayers = participants.map(function(participant){
+              var stats = participant.stats;
+              var player = {
+                champion: {id: participant.championId},
+                summonerSpells: [{ id: participant.spell1Id } , {id : participant.spell2Id} ],
+                summonerName: participant.summonerId,
+                teamId: participant.teamId,
+                role: participant.timeline.lane,
+                kills: stats.kills,
+                deaths: stats.deaths,
+                assists: stats.assists,
+                champLevel: stats.champLevel,
+                creeps: {
+                  ownJungle: stats.neutralMinionsKilledTeamJungle,
+                  enemyJungle: stats.neutralMinionsKilledEnemyJungle,
+                  minions: stats.minionsKilled
+                },
+                goldEarned: stats.goldEarned,
+                items: [
+                  stats.item0,
+                  stats.item1,
+                  stats.item2,
+                  stats.item3,
+                  stats.item4,
+                  stats.item5
+                ],
+                trinket: stats.item6,
+                damageDealt: stats.totalDamageDealtToChampions,
+                wardsPlaced: stats.wardsPlaced,
+                wardsKilled: stats.wardsKilled,
+                winner: stats.winner
+              }
+
+              // get chamption name from api
+              Champion.findOne({championId: player.champion.id})
+                .$promise
+                .then(function(resource){
+                  player.champion.name = resource.name;
+                  player.champion.key = resource.key;
+                });
+
+              // get summoner spells
+              player.summonerSpells.forEach(function(spell){
+                SummonerSpell.findOne({spellId: spell.id})
+                  .$promise
+                  .then(function(resource){
+                    spell.name = resource.name;
+                    spell.key = resource.key;
+                    spell.description = resource.description;
+                  });
+              });  
+
+              //console.log(player);
+              return player;
+          });
+
+        });
+      }
+
     }
   }
 }
