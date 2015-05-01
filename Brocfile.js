226e0cbcc511a12a2a5f9e061b014442b5f5c076
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-04-30 23:59:38 -0400

    Adding sass compilation to the build.

--- Brocfile.js
+++ Brocfile.js
@@ -1,13 +1,18 @@
-var babelTranspiler     =   require('broccoli-babel-transpiler'),
-    sass                =   require('broccoli-sass'),
-    mergeTrees          =   require('broccoli-merge-trees'),
-    funnel              =   require('broccoli-funnel');
+const   babelTranspiler     =   require('broccoli-babel-transpiler'),
+        sass                =   require('broccoli-sass'),
+        mergeTrees          =   require('broccoli-merge-trees'),
+        funnel              =   require('broccoli-funnel');
 
+const   source              =   'source';
+const   public              =   'public';
 
-var source              =   'source';
-var application         =   funnel(source, { include: ['**/*.js'] });
-var compiledJS          =   babelTranspiler(application, {
-                                modules: 'system'
-                            });
+const   styles              =   funnel(source, { include: ['**/*.scss'] });
+const   application         =   funnel(source, { include: ['**/*.js'] });
 
-module.exports = compiledJS;
+var     compiledJS          =   babelTranspiler(application, {
+                                    modules: 'system'
+                                });
+
+var     compiledSASS        =   sass([styles], 'app.scss', 'styles.css');
+
+module.exports              =   mergeTrees([compiledJS, public, compiledSASS]);
