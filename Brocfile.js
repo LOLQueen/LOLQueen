3d95b52af9bb4c0403d49fc1ddf05837555457b1
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-05-01 00:23:49 -0400

    Importing my broccoli-bower-concat plugin. Creating a bower file.

--- Brocfile.js
+++ Brocfile.js
@@ -1,18 +1,21 @@
-const   babelTranspiler     =   require('broccoli-babel-transpiler'),
-        sass                =   require('broccoli-sass'),
-        mergeTrees          =   require('broccoli-merge-trees'),
-        funnel              =   require('broccoli-funnel');
+var babelTranspiler     =   require('broccoli-babel-transpiler'),
+    sass                =   require('broccoli-sass'),
+    mergeTrees          =   require('broccoli-merge-trees'),
+    funnel              =   require('broccoli-funnel'),
+    bowerConcat         =   require('broccoli-bower-concat');
 
-const   source              =   'source';
-const   public              =   'public';
+var source              =   'source';
+var public              =   'public';
+var bower               =   'bower_components';
 
-const   styles              =   funnel(source, { include: ['**/*.scss'] });
-const   application         =   funnel(source, { include: ['**/*.js'] });
+var styles              =   funnel(source, { include: ['**/*.scss'] });
+var application         =   funnel(source, { include: ['**/*.js'] });
 
-var     compiledJS          =   babelTranspiler(application, {
-                                    modules: 'system'
-                                });
+var compiledJS          =   babelTranspiler(application, {
+                                modules: 'system'
+                            });
 
-var     compiledSASS        =   sass([styles], 'app.scss', 'styles.css');
+var compiledSASS        =   sass([styles], 'app.scss', 'styles.css');
+var vendorJS            =   bowerConcat(bower, { extension: 'js' });
 
-module.exports              =   mergeTrees([compiledJS, public, compiledSASS]);
+module.exports          =   mergeTrees([compiledJS, public, compiledSASS]);
