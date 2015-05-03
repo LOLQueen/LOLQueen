3b17c7a5ea7825ac2d0d35380c08de68c5f021c5
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-05-03 03:17:38 -0400

    Extending Summoner#fineOne to allow querying by id.

--- source/shared/resources/summoner/summoner.resource.js
+++ source/shared/resources/summoner/summoner.resource.js
@@ -1,21 +1,44 @@
 'use strict';
 
-import { transform } from './summoner.transformer';
+import {
+    transform
+} from './summoner.transformer';
 
 const store = new WeakMap();
 
 export default class Summoner {
-    
-    constructor ($http, RecentGames, MatchHistory, $q) {
-        store.set(this, { $http, RecentGames, MatchHistory, $q });
+
+    constructor($http, RecentGames, MatchHistory, $q) {
+        store.set(this, {
+            $http, RecentGames, MatchHistory, $q
+        });
     }
 
-    findOne({name, region = 'na'} = {}){
-        const { $http } = this.getDependencies();
+    findOne({ name, id, region = 'na'} = {}) {
 
-        return $http.get(`http://localhost:9000/api/lol/${region}/v1.4/summoner/by-name/${name}`)
-                    .then(getResponseData)
-                    .then(transform.bind(this));
+        if (! (Boolean(name) ^ Boolean(id))) {
+            console.log(name, id);
+            throw new TypeError('Summoner#findOne needs exactly one value of key "name" or "id" in the argument object.');
+        }
+
+        let location = 'http://localhost:9000';
+        
+        if (name) {
+            location += `/api/lol/${region}/v1.4/summoner/by-name/${name}`;
+        }
+
+        if (id) {
+            location += `/api/lol/${region}/v1.4/summoner/${id}`;
+        }
+
+        const {
+            $http
+        } = this.getDependencies();
+
+        return $http
+            .get(location)
+            .then(getResponseData)
+            .then(transform.bind(this));
 
         function getResponseData(response) {
             return response.data[Reflect.ownKeys(response.data)[0]];
